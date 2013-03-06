fb95448c652062f0dc3c0b91abea917ae83c97df
wayde191 <hakula567>
2013-03-06 17:09:59 +0800

    Main structure
    Add structure

---
+++ AppStore/AppStore/iHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  iHAppDelegate.h
+//  AppStore
+//
+//  Created by Wayde Sun on 2/1/13.
+//  Copyright (c) 2013 iHakula. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface iHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AppStore/AppStore/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AppStore
+//
+//  Created by Wayde Sun on 2/1/13.
+//  Copyright (c) 2013 iHakula. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "iHAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([iHAppDelegate class]));
+    }
+}
